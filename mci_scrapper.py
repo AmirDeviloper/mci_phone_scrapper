b56ae4d9032490a48fe7567834160eec622d25a7
Amir HemmatYar <amirdevil.main@gmail.com>
2026-01-29 17:35:50 +0330

    Add files via upload

---
+++ mci_scrapper.py
@@ -0,0 +1,138 @@
+﻿import time
+import requests
+import jdatetime
+import pandas as pd
+from datetime import datetime
+import os
+from phone_checker import PhoneNumberEvaluator
+
+def gregorian_to_jalali(datetime_str: str):
+    if not datetime_str:
+        return None
+    date_str = datetime_str.split('T')[0]
+    g_date = datetime.strptime(date_str, "%Y-%m-%d")
+    j_date = jdatetime.date.fromgregorian(date=g_date)
+    return j_date.strftime("%Y/%m/%d")
+
+
+def get_info_from_mci_ir(output_path: str):
+    url = "https://shop.mci.ir/api/search/v1/products"
+
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://shop.mci.ir/products/3/sim-card?page=0",
+        "platform": "WEB"
+    }
+
+    params = {
+        "category": 3,
+        "size": 16
+    }
+
+    title_map = {
+        "نوع دسته بندی": "الگوی شماره",
+        "نوع فروش": "نوع سیم‌کارت",
+        "نوع خط": "دائمی یا اعتباری"
+    }
+
+    reverse_map = {v: k for k, v in title_map.items()}
+
+    extracted_data = []
+    batch_start_page = 0
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for page in range(0, 625):
+        params["page"] = page
+
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        products = data.get("products", [])
+
+        if not products:
+            print(f"page {page} empty.")
+            break
+
+        for product in products:
+            product_info = {
+                "شماره": product.get("name"),
+                "وضعیت خرید": product.get("productStatus"),
+                "قیمت با مالیات": product.get("priceWithTax"),
+                "زمان درج": gregorian_to_jalali(product.get("createdOn")),
+                **{k: None for k in title_map}
+            }
+
+            for attr in product.get("attributes", []):
+                key = reverse_map.get(attr.get("title"))
+                if key:
+                    product_info[key] = (
+                        attr.get("attributeValueVms", [{}])[0].get("displayText")
+                    )
+
+            extracted_data.append(product_info)
+
+        print(f"page {page} readed.")
+
+        if (page + 1) % 50 == 0:
+            df = pd.DataFrame(extracted_data)
+            filename = fr"{output_path}\sim_cards_page_{batch_start_page}_to_{page}.xlsx"
+            df.to_excel(filename, index=False)
+            print(f"file saved: {filename}")
+
+            extracted_data.clear()
+            batch_start_page = page + 1
+
+        time.sleep(1)
+
+
+    if extracted_data:
+        df = pd.DataFrame(extracted_data)
+        filename = fr"{output_path}\sim_cards_page_{batch_start_page}_to_last.xlsx"
+        df.to_excel(filename, index=False)
+        print(f"final_file_saved: {filename}")
+
+
+
+def merge_excel_folder(folder_path):
+    all_data = []
+
+    excel_files = [f for f in os.listdir(folder_path) if f.endswith((".xlsx", ".xls"))]
+
+    if not excel_files:
+        print("no excel file found.")
+        return
+
+    for file in excel_files:
+        file_path = os.path.join(folder_path, file)
+        try:
+            df = pd.read_excel(file_path, dtype=str)
+            
+            all_data.append(df)
+            print(f"file {file} readed.")
+        except Exception as e:
+            print(f"error while reading {file}: {e}")
+
+    if all_data:
+        merged_df = pd.concat(all_data, ignore_index=True, sort=False)
+        print(f"all files merged.")
+        return merged_df
+    else:
+        print("there is no data for merge.")
+        return None
+
+
+folder_path = "extracted_files"
+get_info_from_mci_ir(folder_path)
+merged_df = merge_excel_folder(folder_path)
+evaluator = PhoneNumberEvaluator()
+scores_df = evaluator.evaluate_score_list(list(merged_df['شماره']))
+result_df = merged_df.merge(scores_df, on='شماره')
+merged_result = 'simcards_score_final.xlsx'
+result_df = result_df.sort_values(by="امتیاز", ascending=False)
+
+result_df.to_excel(merged_result, index=False)
+print(f"final excel file saved: {merged_result}")
---
+++ phone_checker.py
@@ -0,0 +1,102 @@
+﻿import pandas as pd
+
+class PhoneNumberEvaluator:
+    def __init__(self):
+        self.weights = {
+            "تکرار ارقام": 1,
+            "ارقام خطی": 1,
+            "ارقام آینه ای": 1,
+            "ارقام زوج یا فرد": 1,
+            "جفت جفت": 1,
+            "خوشگفتار": 1,
+            "اعداد شانس": 1,
+            "الگوهای خاص": 2,
+            "اعداد گروهی": 1,
+            "تاریخ های خاص": 1,
+            "کمیاب": 2
+        }
+
+    def repeating_digits(self, number):
+        for i in range(len(number)-2):
+            if number[i] == number[i+1] == number[i+2]:
+                return True
+        for i in range(len(number)-3):
+            if number[i] == number[i+1] == number[i+2] == number[i+3]:
+                return True
+        return False
+
+    def sequential_digits(self, number):
+        for i in range(len(number)-2):
+            a, b, c = int(number[i]), int(number[i+1]), int(number[i+2])
+            if b == a+1 and c == b+1:
+                return True
+            if b == a-1 and c == b-1:
+                return True
+        return False
+
+    def mirror_digits(self, number):
+        return number == number[::-1]
+
+    def even_or_odd(self, number):
+        if all(int(d) % 2 == 0 for d in number):
+            return True
+        if all(int(d) % 2 == 1 for d in number):
+            return True
+        return False
+
+    def double_pairs(self, number):
+        for i in range(len(number)-3):
+            if number[i] == number[i+1] and number[i+2] == number[i+3]:
+                return True
+        return False
+
+    def easy_to_remember(self, number):
+        return self.repeating_digits(number) or self.sequential_digits(number) or self.double_pairs(number)
+
+    def lucky_numbers(self, number):
+        lucky_digits = {'7','8','9'}
+        return any(d in lucky_digits for d in number)
+
+    def vip_pattern(self, number):
+        return number.startswith("09") and (self.repeating_digits(number) or self.sequential_digits(number))
+
+    def grouped_numbers(self, number):
+        return self.double_pairs(number)
+
+    def special_dates(self, number):
+        years = ['137','138','139','140'] # examples
+        return any(number.startswith(y) for y in years)
+
+    def rarity(self, number):
+        common_patterns = ['123', '000', '111', '222', '333', '444', '555', '666', '777', '888', '999'] # examples
+        return not any(p in number for p in common_patterns)
+
+    def evaluate_number(self, number):
+        features = {
+            "تکرار ارقام": self.repeating_digits(number),
+            "ارقام خطی": self.sequential_digits(number),
+            "ارقام آینه ای": self.mirror_digits(number),
+            "ارقام زوج یا فرد": self.even_or_odd(number),
+            "جفت جفت": self.double_pairs(number),
+            "خوشگفتار": self.easy_to_remember(number),
+            "اعداد شانس": self.lucky_numbers(number),
+            "الگوهای خاص": self.vip_pattern(number),
+            "اعداد گروهی": self.grouped_numbers(number),
+            "تاریخ های خاص": self.special_dates(number),
+            "کمیاب": self.rarity(number)
+        }
+
+        score = sum(self.weights[f]*v for f,v in features.items())
+        features["امتیاز"] = score
+        features["شماره"] = number
+        return features
+
+    def evaluate_score_list(self, numbers_list):
+        results = [self.evaluate_number(num) for num in numbers_list]
+        df = pd.DataFrame(results)
+
+        for key in self.weights.keys():
+            df[key] = df[key].replace({True: 'دارد', False: 'ندارد'})
+
+        print(f"scores set successfully")
+        return df
